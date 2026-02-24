42ec229a87322451a912458592b85cfa7d958d1b
Liberte Ces <lces@c1r6s5.42london.com>
2026-02-24 16:31:40 +0000

    docstrings

--- Python03/ex0/ft_command_quest.py
+++ Python03/ex0/ft_command_quest.py
@@ -1,14 +1,24 @@
 import sys
 
-args = sys.argv
-print("=== Command Quest ===")
-if len(args) == 1:
-    print("No arguments provided!")
-print("Program name:", args[0])
-if len(args) > 1:
-    print("Arguments received:", len(args) - 1)
-i = 1
-while i < len(args):
-    print(f"Argument {i}: {args[i]}")
-    i += 1
-print("Total arguments:", len(args))
+def demo() -> None:
+    """
+    takes the args from system input and 
+    prints them out, as well as giving
+    the total args given
+    """
+    args = sys.argv
+    print("=== Command Quest ===")
+    if len(args) == 1:
+        print("No arguments provided!")
+    print("Program name:", args[0])
+    if len(args) > 1:
+        print("Arguments received:", len(args) - 1)
+    i = 1
+    while i < len(args):
+        print(f"Argument {i}: {args[i]}")
+        i += 1
+    print("Total arguments:", len(args))
+
+
+demo()
+
